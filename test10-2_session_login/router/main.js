8e52a3bb20328fdd79b36fd6fac37066614bf729
jyanoss <jyanoss@gmail.com>
2022-05-20 16:08:07 +0900

    완료

--- test10-2_session_login/router/main.js
+++ test10-2_session_login/router/main.js
@@ -1,29 +1,34 @@
 module.exports = function(app, fs)
 {
-
+    var userjsonDir = __dirname + "/../data/user.json";
+    
      app.get('/',function(req,res){
+         var sess = req.session;
+
          res.render('index', {
              title: "MY HOMEPAGE",
-             length: 5
+             length: 5,
+             name: sess.name,
+             username: sess.username
          })
      });
 
     app.get('/list', function (req, res) {
-       fs.readFile( __dirname + "/../data/user.json", 'utf8', function (err, data) {
-           console.log( data );
+        
+        fs.readFile( userjsonDir, 'utf8', function (err, data) {
            res.end( data );
        });
     });
 
     app.get('/getUser/:username', function(req, res){
-       fs.readFile( __dirname + "/../data/user.json", 'utf8', function (err, data) {
+       fs.readFile(userjsonDir, 'utf8', function (err, data) {
             var users = JSON.parse(data);
             res.json(users[req.params.username]);
        });
     });
 
     app.post('/addUser/:username', function(req, res){
-
+        
         var result = {  };
         var username = req.params.username;
 
@@ -35,8 +40,8 @@ module.exports = function(app, fs)
             return;
         }
 
-        // LOAD DATA & CHECK DUPLICATION
-        fs.readFile( __dirname + "/../data/user.json", 'utf8',  function(err, data){
+        // LOAD DATA & CHECK DUPLICATION   
+        fs.readFile(userjsonDir, 'utf8',  function(err, data){
             var users = JSON.parse(data);
             if(users[username]){
                 // DUPLICATION FOUND
@@ -50,7 +55,7 @@ module.exports = function(app, fs)
             users[username] = req.body;
 
             // SAVE DATA
-            fs.writeFile(__dirname + "/../data/user.json",
+            fs.writeFile(userjsonDir, 
                          JSON.stringify(users, null, '\t'), "utf8", function(err, data){
                 result = {"success": 1};
                 res.json(result);
@@ -58,9 +63,9 @@ module.exports = function(app, fs)
         })
     });
 
-
+    
     app.put('/updateUser/:username', function(req, res){
-
+        
         var result = {  };
         var username = req.params.username;
 
@@ -72,14 +77,14 @@ module.exports = function(app, fs)
             return;
         }
 
-        // LOAD DATA
-        fs.readFile( __dirname + "/../data/user.json", 'utf8',  function(err, data){
+        // LOAD DATA   
+        fs.readFile(userjsonDir, 'utf8',  function(err, data){
             var users = JSON.parse(data);
             // ADD/MODIFY DATA
             users[username] = req.body;
 
             // SAVE DATA
-            fs.writeFile(__dirname + "/../data/user.json",
+            fs.writeFile(userjsonDir, 
                          JSON.stringify(users, null, '\t'), "utf8", function(err, data){
                 result = {"success": 1};
                 res.json(result);
@@ -91,9 +96,9 @@ module.exports = function(app, fs)
     app.delete('/deleteUser/:username', function(req, res){
         var result = { };
         //LOAD DATA
-        fs.readFile(__dirname + "/../data/user.json", "utf8", function(err, data){
+        fs.readFile(userjsonDir, "utf8", function(err, data){
             var users = JSON.parse(data);
-
+            
             // IF NOT FOUND
             if(!users[req.params.username]){
                 result["success"] = 0;
@@ -104,9 +109,9 @@ module.exports = function(app, fs)
 
             // DELETE FROM DATA
             delete users[req.params.username];
-
+            
             // SAVE FILE
-            fs.writeFile(__dirname + "/../data/user.json",
+            fs.writeFile(userjsonDir,
                          JSON.stringify(users, null, '\t'), "utf8", function(err, data){
                 result["success"] = 1;
                 res.json(result);
@@ -114,6 +119,51 @@ module.exports = function(app, fs)
             })
         })
 
-    })
+    });
 
+    app.get('/login/:username/:password', function(req, res){
+        var sess;
+        sess = req.session;
+
+        fs.readFile(userjsonDir, "utf8", function(err, data){
+            var users = JSON.parse(data);
+            var username = req.params.username;
+            var password = req.params.password;
+            var result = {};
+            if(!users[username]){
+                // USERNAME NOT FOUND
+                result["success"] = 0;
+                result["error"] = "not found";
+                res.json(result);
+                return;
+            }     
+
+            if(users[username]["password"] == password){
+                result["success"] = 1;
+                sess.username = username;
+                sess.name = users[username]["name"];
+                res.json(result);
+            
+            }else{
+                result["success"] = 0;
+                result["error"] = "incorrect";
+                res.json(result);
+            }
+        })
+    });
+
+    app.get('/logout', function(req, res){
+        sess = req.session;
+        if(sess.username){
+            req.session.destroy(function(err){
+                if(err){
+                    console.log(err);
+                }else{
+                    res.redirect('/');
+                }
+            })
+        }else{
+            res.redirect('/');
+        }
+    })
 }
--- test10-2_session_login/server.js
+++ test10-2_session_login/server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var session = require('express-session');
-var fs = require("fs")
+var fs = require("fs");
 
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
@@ -10,17 +10,18 @@ app.engine('html', require('ejs').renderFile);
 
 
 var server = app.listen(3000, function(){
- console.log("Express server has started on port 3000")
+    console.log("Express server has started on port 3000")
 });
 
 app.use(express.static('public'));
 
 app.use(bodyParser.json());
+app.use(bodyParser.text());
 app.use(bodyParser.urlencoded());
 app.use(session({
- secret: '@#@$MYSIGN#@$#$',
- resave: false,
- saveUninitialized: true
+    secret: '@#@$MYSIGN#@$#$',
+    resave: false,
+    saveUninitialized: true
 }));
 
 
