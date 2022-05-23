d4fc2b3350127ca03d6fdaae7bf3dddae8f1e13a
jyanoss <jyanoss@gmail.com>
2022-05-23 18:44:51 +0900

    작업중

--- node.js-mysql-1/main.js
+++ node.js-mysql-1/main.js
@@ -1,10 +1,10 @@
-var http = require('http');
-var fs = require('fs');
-var url = require('url');
-var qs = require('querystring');
-var template = require('./lib/template.js');
-var path = require('path');
-var sanitizeHtml = require('sanitize-html');
+var http          = require('http');
+var fs            = require('fs');
+var url           = require('url');
+var qs            = require('querystring');
+var template      = require('./lib/template.js');
+var path          = require('path');
+var sanitizeHtml  = require('sanitize-html');
 
 var mysql     = require('mysql');
 var db        = mysql.createConnection({
@@ -21,30 +21,22 @@ var app = http.createServer(function(request,response){
     var pathname = url.parse(_url, true).pathname;
     if(pathname === '/'){
       if(queryData.id === undefined){
-        /*
-        fs.readdir('./data', function(error, filelist){
-          var title = 'Welcome';
-          var description = 'Hello, Node.js';
-          var list = template.list(filelist);
-          var html = template.HTML(title, list,
-            `<h2>${title}</h2>${description}`,
-            `<a href="/create">create</a>`
-          );
-          response.writeHead(200);
-          response.end(html);
-        });
-        */
-
+ 
         db.query(`select * from topic`, function(error,topics){
-            console.log(topics);
+            var title = 'Welcome';
+            var description = 'Hello, Node.js';
+            var list = template.list(topics);
+            var html = template.HTML(title, list,
+              `<h2>${title}</h2>${description}`,
+              `<a href="/create">create</a>`
+            );
             response.writeHead(200);
-            response.end('Success');
+            response.end(html);
         });
-
-
 
 
       } else {
+/*
         fs.readdir('./data', function(error, filelist){
           var filteredId = path.parse(queryData.id).base;
           fs.readFile(`data/${filteredId}`, 'utf8', function(err, description){
@@ -67,6 +59,27 @@ var app = http.createServer(function(request,response){
             response.end(html);
           });
         });
+*/
+        db.query(`select * from topic`, function(error,topics){
+          if (error){
+            throw error;
+          }
+          db.query(`select * from topic where id = ?`, [queryData.id], function(error2,topic){
+            if (error2){
+              throw error2;
+            }
+
+          var title = topic[0].title;
+          var description = topic[0].description;
+          var list = template.list(topics);
+          var html = template.HTML(title, list,
+            `<h2>${title}</h2>${description}`,
+            `<a href="/create">create</a>`
+          );
+          response.writeHead(200);
+          response.end(html);
+          })
+        });       
       }
     } else if(pathname === '/create'){
       fs.readdir('./data', function(error, filelist){
@@ -161,4 +174,5 @@ var app = http.createServer(function(request,response){
       response.end('Not found');
     }
 });
+
 app.listen(3000);
