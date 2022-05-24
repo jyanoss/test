890d8b8e496d2cc456f08023335396bbf6567374
jyanoss <jyanoss@gmail.com>
2022-05-24 18:20:40 +0900

    111

--- node.js-mysql-1/main.js
+++ node.js-mysql-1/main.js
@@ -16,17 +16,18 @@ var db        = mysql.createConnection({
 db.connect();
 
 var app = http.createServer(function(request,response){
-    var _url = request.url;
+    var _url      = request.url;
     var queryData = url.parse(_url, true).query;
-    var pathname = url.parse(_url, true).pathname;
+    var pathname  = url.parse(_url, true).pathname;
+
     if(pathname === '/'){
       if(queryData.id === undefined){
         //리스트 보기
         db.query(`select * from topic`, function(error,topics){
-            var title = 'List';
+            var title       = 'List';
             var description = 'Hello, Node.js';
-            var list = template.list(topics);
-            var html = template.HTML(title, list,
+            var list        = template.list(topics);
+            var html        = template.HTML(title, list,
               `<h2>${title}</h2>${description}`,
               `<a href="/create">create</a>`
             );
@@ -41,6 +42,7 @@ var app = http.createServer(function(request,response){
           if (error){
             throw error;
           }
+        
           db.query(`select * from topic where id = ?`, [queryData.id], function(error2,topic){
             if (error2){
               throw error2;
@@ -86,7 +88,7 @@ var app = http.createServer(function(request,response){
       });
 
     } else if(pathname === '/create_process'){
-      //글생성프로세스
+      //글생성 프로세스
       var body = '';
       request.on('data', function(data){
           body = body + data;
@@ -95,58 +97,75 @@ var app = http.createServer(function(request,response){
           var post        = qs.parse(body);
           var title       = post.title;
           var description = post.description;
-          /*
-          fs.writeFile(`data/${title}`, description, 'utf8', function(err){
-            response.writeHead(302, {Location: `/?id=${title}`});
-            response.end();
-          })
-          */
-        console.log(post)
+          db.query(`
+            insert into topic (title, description, created, author_id) 
+            values ( ?, ?, NOW(), ?)`, 
+            [post.title, post.description,1] , 
+            function(error, result){
+              if(error){
+                throw error;
+              }
+              response.writeHead(302, {Location: `/?id=${result.insertId}`});
+              response.end();
+            }
+          )         
       });
 
     } else if(pathname === '/update'){
-      fs.readdir('./data', function(error, filelist){
-        var filteredId = path.parse(queryData.id).base;
-        fs.readFile(`data/${filteredId}`, 'utf8', function(err, description){
-          var title = queryData.id;
-          var list = template.list(filelist);
-          var html = template.HTML(title, list,
+      //글수정
+        db.query(`select * from topic`, function(error,topics){
+          if (error){
+            throw error;
+          }
+        
+          db.query(`select * from topic where id = ?`, [queryData.id], function(error2,topic){
+            if (error2){
+              throw error2;
+            }
+
+          var list    = template.list(topics);
+          var html    = template.HTML(topic[0].title, list,
             `
             <form action="/update_process" method="post">
-              <input type="hidden" name="id" value="${title}">
-              <p><input type="text" name="title" placeholder="title" value="${title}"></p>
+              <input type="hidden" name="id" value="${topic[0].id}">
+              <p><input type="text" name="title" placeholder="title" value="${topic[0].title}"></p>
               <p>
-                <textarea name="description" placeholder="description">${description}</textarea>
+                <textarea name="description" placeholder="description">${topic[0].description}</textarea>
               </p>
               <p>
                 <input type="submit">
               </p>
             </form>
             `,
-            `<a href="/create">create</a> <a href="/update?id=${title}">update</a>`
+            `<a href="/create">create</a> <a href="/update_process?id=${topic[0].id}">update</a>`
           );
           response.writeHead(200);
           response.end(html);
-        });
-      });
+          })
+        });     
+        
     } else if(pathname === '/update_process'){
+      //글수정프로세스
       var body = '';
       request.on('data', function(data){
           body = body + data;
       });
+      
       request.on('end', function(){
-          var post = qs.parse(body);
-          var id = post.id;
-          var title = post.title;
-          var description = post.description;
-          fs.rename(`data/${id}`, `data/${title}`, function(error){
-            fs.writeFile(`data/${title}`, description, 'utf8', function(err){
-              response.writeHead(302, {Location: `/?id=${title}`});
-              response.end();
-            })
-          });
+          var post        = qs.parse(body);
+          db.query(
+            'UPDATE topic SET title=?, description=?, author_id=1 WHERE id=?'
+            , [post.title, post.description, post.id], function(error, result){
+            response.writeHead(302, {Location: `/?id=${post.id}`});
+            response.end();
+            }
+          )
+ 
       });
+
     } else if(pathname === '/delete_process'){
+      
+      //글삭제
       var body = '';
       request.on('data', function(data){
           body = body + data;
@@ -160,6 +179,7 @@ var app = http.createServer(function(request,response){
             response.end();
           })
       });
+
     } else {
       response.writeHead(404);
       response.end('Not found');
